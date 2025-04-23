c253c283660e4bf26aaa9e8b7d8318c5fac426e9
matthewkeil <me@matthewkeil.com>
2025-04-24 04:26:45 +0700

    fix: regression from adding logger to SeenGossipBlockInput

---
+++ packages/beacon-node/src/chain/blocks/blockInput/testBlockInputTypes.ts
@@ -0,0 +1,29 @@
+// import {ForkPostDeneb} from "@lodestar/params";
+// import {SignedBeaconBlock} from "@lodestar/types";
+// import {BlockInput, isBlockInputBlobs, isBlockInputPreData} from "./blockInput.js";
+// import {
+//   BlockInputBlobsProps,
+//   // BlockInputColumnsProps
+// } from "./types.js";
+// import {
+//   // isBlockInputBlobs,
+//   // isBlockInputColumns,
+//   // isBlockInputPreDeneb,
+// } from "./utils.js";
+
+// export function testTypes(): void {
+//   const blockInputs: BlockInput[] = [];
+
+//   blockInputs.push(new BlockInput({} as any));
+
+//   for (const blockInput of blockInputs) {
+//     if (isBlockInputPreData(blockInput)) {
+//       blockInputs.push(blockInput);
+//     } else if (isBlockInputBlobs(blockInput)) {
+//       blockInput;
+//       blockInputs.push(blockInput);
+//       // } else if (isBlockInputColumns(blockInput)) {
+//       //   blockInputs.push(blockInput);
+//     }
+//   }
+// }
