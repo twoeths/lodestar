745b305636266838b5f5fc64aea332a32408d388
Derek Guenther <derek.guenther@coinbase.com>
2025-04-24 09:58:22 -0400

    fix: always add CGC to ENR (#7738)
    **Motivation**
    
    The MetadataController currently sets CGC on the ENR in two cases:
    
    * When the node is first started after the Fulu epoch
    * Any time `setAdvertisedGroupCount` is called
    
    We should at least ensure CGC is set when crossing the Fulu fork
    boundary so that nodes are aware of our CGC (for example, if we're set
    as a supernode, nodes would currently assume we only custody 4 columns).
    
    Additionally, I think we should have CGC available prior to the fork
    boundary in case nodes want to use it to prioritize peers before the
    fork. We already do this for Metadata by making the MetadataV3 endpoint
    available prior to the fork.
    
    <!-- Why is this PR exists? What are the goals of the pull request? -->
    
    **Description**
    
    * Removes the condition around CGC in the upstreamValues function so
    that CGC is sent to ENR regardless of the epoch at node start.
    * Adds a test to make sure CGC is always set by upstreamValues

--- packages/beacon-node/src/network/metadata.ts
+++ packages/beacon-node/src/network/metadata.ts
@@ -61,12 +61,8 @@ export class MetadataController {
       this.onSetValue(ENRKey.syncnets, ssz.phase0.AttestationSubnets.serialize(this._metadata.syncnets));
     }
 
-    if (config.getForkSeq(computeStartSlotAtEpoch(currentEpoch)) >= ForkSeq.fulu) {
-      this.onSetValue(
-        ENRKey.cgc,
-        intToBytes(this._metadata.cgc, Math.ceil(Math.log2(this._metadata.cgc + 1) / 8), "be")
-      );
-    }
+    // Set CGC regardless of fork. It may be useful to clients before Fulu, and will be ignored otherwise.
+    this.onSetValue(ENRKey.cgc, intToBytes(this._metadata.cgc, Math.ceil(Math.log2(this._metadata.cgc + 1) / 8), "be"));
   }
 
   get seqNumber(): bigint {
