twoeths/lodestar

commit 899163f126dc6ec7cf7b21d3abc1d5524a918048

fix: fix proposer boost reorg by suppressing fcu call during import (#7810)
We need to suppress fcu call during gossip block import when the
incoming block is weak. This is because once notified EL, it will treat
the new block as head and will not reorg back to parent if we try to
reorg.

- Add `shouldOverrideForkChoiceUpdate`
- Call `shouldOverrideForkChoiceUpdate` during gossip block import, and
skip fcu call if result is true
- Modify `predictProposerHead` to be a simple wrapper around
`shouldOverrideForkChoiceUpdate`


Closes #7235

cc. @twoeths @nflaig

---------

Co-authored-by: Nico Flaig <nflaig@protonmail.com>

packages/beacon-node/src/chain/chain.ts
@@ -841,9 +841,10 @@ export class BeaconChain implements IBeaconChain {
   predictProposerHead(slot: Slot): ProtoBlock {
     this.metrics?.forkChoice.requests.inc();
     const timer = this.metrics?.forkChoice.findHead.startTimer({caller: FindHeadFnName.predictProposerHead});
+    const secFromSlot = this.clock.secFromSlot(slot);
 
     try {
-      return this.forkChoice.updateAndGetHead({mode: UpdateHeadOpt.GetPredictedProposerHead, slot}).head;
+      return this.forkChoice.updateAndGetHead({mode: UpdateHeadOpt.GetPredictedProposerHead, secFromSlot, slot}).head;
     } catch (e) {
       this.metrics?.forkChoice.errors.inc({entrypoint: UpdateHeadOpt.GetPredictedProposerHead});
       throw e;

packages/fork-choice/src/forkChoice/forkChoice.ts
@@ -52,6 +52,7 @@ import {
   LatestMessage,
   NotReorgedReason,
   PowBlockHex,
+  ShouldOverrideForkChoiceUpdateResult,
 } from "./interface.js";
 import {CheckpointWithHex, IForkChoiceStore, JustifiedBalances, toCheckpointWithHex} from "./store.js";
 
@@ -70,7 +71,7 @@ export enum UpdateHeadOpt {
 export type UpdateAndGetHeadOpt =
   | {mode: UpdateHeadOpt.GetCanonicialHead}
   | {mode: UpdateHeadOpt.GetProposerHead; secFromSlot: number; slot: Slot}
-  | {mode: UpdateHeadOpt.GetPredictedProposerHead; slot: Slot};
+  | {mode: UpdateHeadOpt.GetPredictedProposerHead; secFromSlot: number; slot: Slot};
 
 /**
  * Provides an implementation of "Ethereum Consensus -- Beacon Chain Fork Choice":
@@ -207,7 +208,7 @@ export class ForkChoice implements IForkChoice {
     const canonicialHeadBlock = mode === UpdateHeadOpt.GetPredictedProposerHead ? this.getHead() : this.updateHead();
     switch (mode) {
       case UpdateHeadOpt.GetPredictedProposerHead:
-        return {head: this.predictProposerHead(canonicialHeadBlock, opt.slot)};
+        return {head: this.predictProposerHead(canonicialHeadBlock, opt.secFromSlot, opt.slot)};
       case UpdateHeadOpt.GetProposerHead: {
         const {
           proposerHead: head,
@@ -221,6 +222,60 @@ export class ForkChoice implements IForkChoice {
       default:
         return {head: canonicialHeadBlock};
     }
+  }
+
+  // Called by `predictProposerHead` and `importBlock`. If the result is not same as blockRoot's block, return true else false
+  // See https://github.com/ethereum/consensus-specs/blob/v1.5.0/specs/bellatrix/fork-choice.md#should_override_forkchoice_update
+  // Return true if the given block passes all criteria to be re-orged out
+  // Return false otherwise.
+  // Note when proposer boost reorg is disabled, it always returns false
+  shouldOverrideForkChoiceUpdate(
+    blockRoot: RootHex,
+    secFromSlot: number,
+    currentSlot: Slot
+  ): ShouldOverrideForkChoiceUpdateResult {
+    const headBlock = this.getBlockHex(blockRoot);
+    if (headBlock === null) {
+      // should not happen because this block just got imported. Fall back to no-reorg.
+      return {shouldOverrideFcu: false, reason: NotReorgedReason.HeadBlockNotAvailable};
+    }
+    const {proposerBoost, proposerBoostReorg} = this.opts ?? {};
+    // Skip re-org attempt if proposer boost (reorg) are disabled
+    if (!proposerBoost || !proposerBoostReorg) {
+      this.logger?.verbose("Skip shouldOverrideForkChoiceUpdate check since the related flags are disabled", {
+        slot: currentSlot,
+        proposerBoost,
+        proposerBoostReorg,
+      });
+      return {shouldOverrideFcu: false, reason: NotReorgedReason.ProposerBoostReorgDisabled};
+    }
+
+    const parentBlock = this.protoArray.getBlock(headBlock.parentRoot);
+    const proposalSlot = headBlock.slot + 1;
+
+    // No reorg if parentBlock isn't available
+    if (parentBlock === undefined) {
+      return {shouldOverrideFcu: false, reason: NotReorgedReason.ParentBlockNotAvailable};
+    }
+
+    const {prelimProposerHead, prelimNotReorgedReason} = this.getPreliminaryProposerHead(
+      headBlock,
+      parentBlock,
+      proposalSlot
+    );
+
+    if (prelimProposerHead === headBlock) {
+      return {shouldOverrideFcu: false, reason: prelimNotReorgedReason ?? NotReorgedReason.Unknown};
+    }
+
+    const currentTimeOk =
+      headBlock.slot === currentSlot || (proposalSlot === currentSlot && this.isProposingOnTime(secFromSlot));
+    if (!currentTimeOk) {
+      return {shouldOverrideFcu: false, reason: NotReorgedReason.ReorgMoreThanOneSlot};
+    }
+
+    this.logger?.verbose("Block is weak. Should override forkchoice update", {blockRoot, slot: currentSlot});
+    return {shouldOverrideFcu: true, parentBlock};
   }
 
   /**
@@ -239,37 +294,32 @@ export class ForkChoice implements IForkChoice {
    *
    * By calling this function, we assume we are the proposer of next slot
    *
-   * https://github.com/ethereum/consensus-specs/blob/v1.4.0-beta.4/specs/bellatrix/fork-choice.md#should_override_forkchoice_update
    */
-  predictProposerHead(headBlock: ProtoBlock, currentSlot?: Slot): ProtoBlock {
+  predictProposerHead(headBlock: ProtoBlock, secFromSlot: number, currentSlot: Slot): ProtoBlock {
     // Skip re-org attempt if proposer boost (reorg) are disabled
     if (!this.opts?.proposerBoost || !this.opts?.proposerBoostReorg) {
       this.logger?.verbose("No proposer boot reorg prediction since the related flags are disabled");
       return headBlock;
     }
 
-    const parentBlock = this.protoArray.getBlock(headBlock.parentRoot);
-    const proposalSlot = headBlock.slot + 1;
-    currentSlot = currentSlot ?? this.fcStore.currentSlot;
-
-    // No reorg if parentBlock isn't available
-    if (parentBlock === undefined) {
+    if (headBlock.slot === currentSlot) {
+      // If head block aka the head cache is current, that means `updateHead` is called during gossip handling,
+      // that can only happen if shouldOverrideForkChoiceUpdate = false so no reorg
       return headBlock;
     }
 
-    const {prelimProposerHead} = this.getPreliminaryProposerHead(headBlock, parentBlock, proposalSlot);
+    const blockRoot = headBlock.blockRoot;
+    const result = this.shouldOverrideForkChoiceUpdate(blockRoot, secFromSlot, currentSlot);
 
-    if (prelimProposerHead === headBlock) {
-      return headBlock;
+    if (result.shouldOverrideFcu) {
+      this.logger?.verbose("Current head is weak. Predicting next block to be built on parent of head.", {
+        blockRoot,
+        slot: currentSlot,
+      });
+      return result.parentBlock;
     }
 
-    const currentTimeOk = headBlock.slot === currentSlot;
-    if (!currentTimeOk) {
-      return headBlock;
-    }
-
-    this.logger?.info("Current head is weak. Predicting next block to be built on parent of head");
-    return parentBlock;
+    return headBlock;
   }
 
   /**
@@ -307,10 +357,8 @@ export class ForkChoice implements IForkChoice {
       return {proposerHead, isHeadTimely, notReorgedReason: prelimNotReorgedReason};
     }
 
-    // https://github.com/ethereum/consensus-specs/blob/v1.4.0-beta.4/specs/phase0/fork-choice.md#is_proposing_on_time
-    const proposerReorgCutoff = this.config.SECONDS_PER_SLOT / INTERVALS_PER_SLOT / 2;
-    const isProposingOnTime = secFromSlot <= proposerReorgCutoff;
-    if (!isProposingOnTime) {
+    // Only re-org if we are proposing on-time
+    if (!this.isProposingOnTime(secFromSlot)) {
       return {proposerHead, isHeadTimely, notReorgedReason: NotReorgedReason.NotProposingOnTime};
     }
 
@@ -1131,6 +1179,14 @@ export class ForkChoice implements IForkChoice {
   protected isBlockTimely(block: BeaconBlock, blockDelaySec: number): boolean {
     const isBeforeAttestingInterval = blockDelaySec < this.config.SECONDS_PER_SLOT / INTERVALS_PER_SLOT;
     return this.fcStore.currentSlot === block.slot && isBeforeAttestingInterval;
+  }
+
+  /**
+   * https://github.com/ethereum/consensus-specs/blob/v1.5.0/specs/phase0/fork-choice.md#is_proposing_on_time
+   */
+  private isProposingOnTime(secFromSlot: number): boolean {
+    const proposerReorgCutoff = this.config.SECONDS_PER_SLOT / INTERVALS_PER_SLOT / 2;
+    return secFromSlot <= proposerReorgCutoff;
   }
 
   private getPreMergeExecStatus(executionStatus: MaybeValidExecutionStatus): ExecutionStatus.PreMerge {

packages/beacon-node/src/chain/blocks/importBlock.ts
@@ -1,6 +1,12 @@
 import {BitArray} from "@chainsafe/ssz";
 import {routes} from "@lodestar/api";
-import {AncestorStatus, EpochDifference, ForkChoiceError, ForkChoiceErrorCode} from "@lodestar/fork-choice";
+import {
+  AncestorStatus,
+  EpochDifference,
+  ForkChoiceError,
+  ForkChoiceErrorCode,
+  NotReorgedReason,
+} from "@lodestar/fork-choice";
 import {
   ForkPostAltair,
   ForkPostElectra,
@@ -15,6 +21,8 @@ import {
   RootCache,
   computeEpochAtSlot,
   computeStartSlotAtEpoch,
+  isExecutionStateType,
+  isStartSlotOfEpoch,
   isStateValidatorsNodesPopulated,
 } from "@lodestar/state-transition";
 import {Attestation, BeaconBlock, altair, capella, electra, phase0, ssz} from "@lodestar/types";
@@ -313,9 +321,64 @@ export async function importBlock(
   // Notifying EL of head and finalized updates as below is usually done within the 1st 4s of the slot.
   // If there is an advanced payload generation in the next slot, we'll notify EL again 4s before next
   // slot via PrepareNextSlotScheduler. There is no harm updating the ELs with same data, it will just ignore it.
+
+  // Suppress fcu call if shouldOverrideFcu is true. This only happens if we have proposer boost reorg enabled
+  // and the block is weak and can potentially be reorged out.
+  let shouldOverrideFcu = false;
+  let notOverrideFcuReason = NotReorgedReason.Unknown;
+
+  if (opts.isGossipBlock && isExecutionStateType(postState)) {
+    const proposalSlot = blockSlot + 1;
+    try {
+      const proposerIndex = postState.epochCtx.getBeaconProposer(proposalSlot);
+      const feeRecipient = this.beaconProposerCache.get(proposerIndex);
+      const {currentSlot} = this.clock;
+
+      if (feeRecipient) {
+        // We would set this to true if
+        //  1) This is a gossip block
+        //  2) We are proposer of next slot
+        //  3) Proposer boost reorg related flag is turned on (this is checked inside the function)
+        //  4) Block meets the criteria of being re-orged out (this is also checked inside the function)
+        const result = this.forkChoice.shouldOverrideForkChoiceUpdate(
+          blockSummary.blockRoot,
+          this.clock.secFromSlot(currentSlot),
+          currentSlot
+        );
+        shouldOverrideFcu = result.shouldOverrideFcu;
+        if (!result.shouldOverrideFcu) {
+          notOverrideFcuReason = result.reason;
+        }
+      } else {
+        notOverrideFcuReason = NotReorgedReason.NotProposerOfNextSlot;
+      }
+    } catch (e) {
+      if (isStartSlotOfEpoch(proposalSlot)) {
+        notOverrideFcuReason = NotReorgedReason.NotShufflingStable;
+      } else {
+        this.logger.warn("Unable to get beacon proposer. Do not override fcu.", {proposalSlot}, e as Error);
+      }
+    }
+  }
+
+  if (shouldOverrideFcu) {
+    this.logger.verbose("Weak block detected. Skip fcu call in importBlock", {
+      blockRoot: blockRootHex,
+      slot: blockSlot,
+    });
+  } else {
+    this.metrics?.importBlock.notOverrideFcuReason.inc({reason: notOverrideFcuReason});
+    this.logger.verbose("Strong block detected. Not override fcu call", {
+      blockRoot: blockRootHex,
+      slot: blockSlot,
+      reason: notOverrideFcuReason,
+    });
+  }
+
   if (
     !this.opts.disableImportExecutionFcU &&
-    (newHead.blockRoot !== oldHead.blockRoot || currFinalizedEpoch !== prevFinalizedEpoch)
+    (newHead.blockRoot !== oldHead.blockRoot || currFinalizedEpoch !== prevFinalizedEpoch) &&
+    !shouldOverrideFcu
   ) {
     /**
      * On post BELLATRIX_EPOCH but pre TTD, blocks include empty execution payload with a zero block hash.

packages/beacon-node/src/chain/blocks/types.ts
@@ -196,6 +196,8 @@ export type ImportBlockOpts = {
   seenTimestampSec?: number;
   /** Set to true if persist block right at verification time */
   eagerPersistBlock?: boolean;
+  /** Set to true if the importing block is from gossip */
+  isGossipBlock?: boolean;
 };
 
 /**

packages/beacon-node/src/metrics/metrics/lodestar.ts
@@ -1,3 +1,4 @@
+import {NotReorgedReason} from "@lodestar/fork-choice";
 import {BlockInputSource} from "../../chain/blocks/blockInput/index.js";
 import {BlobsSource, BlockSource} from "../../chain/blocks/types.js";
 import {JobQueueItemType} from "../../chain/bls/index.js";
@@ -743,6 +744,11 @@ export function createLodestarMetrics(
         name: "lodestar_import_blobs_by_source_total",
         help: "Total number of imported blobs by source",
         labelNames: ["blobsSource"],
+      }),
+      notOverrideFcuReason: register.counter<{reason: NotReorgedReason}>({
+        name: "lodestar_import_block_not_override_fcu_reason_total",
+        help: "Reason why the fcu call is not suppressed during block import",
+        labelNames: ["reason"],
       }),
     },
     engineNotifyNewPayloadResult: register.gauge<{result: ExecutionPayloadStatus}>({

packages/beacon-node/src/network/processor/gossipHandlers.ts
@@ -303,6 +303,7 @@ function getSequentialHandlers(modules: ValidatorFnsModules, options: GossipHand
         seenTimestampSec,
         // gossip block is validated, we want to process it asap
         eagerPersistBlock: true,
+        isGossipBlock: true,
       })
       .then(() => {
         // Returns the delay between the start of `block.slot` and `current time`

packages/beacon-node/test/spec/presets/fork_choice.test.ts
@@ -324,6 +324,21 @@ const forkChoiceTest =
                   `Invalid proposer head at step ${i}`
                 );
               }
+              if (step.checks.should_override_forkchoice_update) {
+                const currentSlot = Math.floor(tickTime / config.SECONDS_PER_SLOT);
+                const result = chain.forkChoice.shouldOverrideForkChoiceUpdate(
+                  head.blockRoot,
+                  tickTime % config.SECONDS_PER_SLOT,
+                  currentSlot
+                );
+                if (result.shouldOverrideFcu === false) {
+                  logger.debug(`Not override fcu reason ${result.reason} at step ${i}`);
+                }
+                expect({result: result.shouldOverrideFcu, validator_is_connected: true}).toEqualWithMessage(
+                  step.checks.should_override_forkchoice_update,
+                  `Invalid should override fcu result at step ${i}`
+                );
+              }
             }
 
             // None of the above
@@ -487,6 +502,10 @@ type Checks = {
     finalized_checkpoint?: SpecTestCheckpoint;
     proposer_boost_root?: RootHex;
     get_proposer_head?: string;
+    should_override_forkchoice_update?: {
+      validator_is_connected: boolean;
+      result: boolean;
+    };
   };
 };
 

packages/fork-choice/src/forkChoice/interface.ts
@@ -63,8 +63,11 @@ export enum NotReorgedReason {
   ReorgMoreThanOneSlot = "reorgMoreThanOneSlot",
   ProposerBoostNotWornOff = "proposerBoostNotWornOff",
   HeadBlockNotWeak = "headBlockNotWeak",
-  ParentBlockNotStrong = "ParentBlockNotStrong",
+  ParentBlockNotStrong = "parentBlockNotStrong",
   NotProposingOnTime = "notProposingOnTime",
+  NotProposerOfNextSlot = "notProposerOfNextSlot",
+  HeadBlockNotAvailable = "headBlockNotAvailable", // Should not happen because head block should be in cache
+  Unknown = "unknown", // A placeholder in case reason is not provided
 }
 
 export type ForkChoiceMetrics = {
@@ -75,6 +78,10 @@ export type ForkChoiceMetrics = {
   nodes: number;
   indices: number;
 };
+
+export type ShouldOverrideForkChoiceUpdateResult =
+  | {shouldOverrideFcu: true; parentBlock: ProtoBlock}
+  | {shouldOverrideFcu: false; reason: NotReorgedReason};
 
 export interface IForkChoice {
   irrecoverableError?: Error;
@@ -107,6 +114,16 @@ export interface IForkChoice {
     isHeadTimely?: boolean;
     notReorgedReason?: NotReorgedReason;
   };
+  /**
+   * This is called during block import when proposerBoostReorg is enabled
+   * fcu call in `importBlock()` will be suppressed if this returns true. It is also
+   * called by `predictProposerHead()` during `prepareNextSlot()`.
+   */
+  shouldOverrideForkChoiceUpdate(
+    blockRoot: RootHex,
+    secFromSlot: number,
+    currentSlot: Slot
+  ): ShouldOverrideForkChoiceUpdateResult;
   /**
    * Retrieves all possible chain heads (leaves of fork choice tree).
    */

commit ea1977daefa669ebf17d639feb4d8d462ba20db6

fix: detect weak head when predicting proposer head (#8079)
**Motivation**

There was a regression in
https://github.com/ChainSafe/lodestar/pull/7810 which caused us to not
detect weak head when predicting proposer head.

**Description**

This PR addresses the issue to correctly detect weak head when
predicting proposer head and updates few logs / misc changes while
reviewing the code.

I tested this on Kurtosis by artificially delaying some blocks. Let's
look at the example of proposer for slot 54 which re-orged out the late
block from slot 53.

The block was received after 4 seconds into the slot via gossip
```
Received gossip block slot=53, root=0x77cc…92e1, currentSlot=53, peerId=16Uiu2HAmQ8AL3EKjxSs7V6AG1CA4xMXvWXnVEZ6jybSShxCX48Fy, delaySec=4.1579999923706055, pending=null, haveBlobs=0, expectedBlobs=0, recvToValLatency=0.003000020980834961
```
We correctly determined during block import that it's a weak block and
skipped fcu call
```
Block is weak. Should override forkchoice update blockRoot=0x77cc3e39bc0c843f25c9556141f30e16901516a49dfb956fdc68e9c6f98592e1, slot=53
Weak block detected. Skip fcu call in importBlock blockRoot=0x77cc3e39bc0c843f25c9556141f30e16901516a49dfb956fdc68e9c6f98592e1, slot=53
```
When preparing next slot, we predicted that next block will be built on
parent (at start of next slot)
```
Current head is weak. Predicting next block to be built on parent of head. slot=53, weakHead=0x77cc3e39bc0c843f25c9556141f30e16901516a49dfb956fdc68e9c6f98592e1, proposerHead=0xfe8dfdd3ebc9715bbf9fac23d14024cf9dc29bfe78d352a9ea7eeea5f8cd25f1
```
Which turned out to be the case and we re-orged out the weak head
```
Performing single-slot reorg to remove current weak head slot=54, proposerHead=0xfe8dfdd3ebc9715bbf9fac23d14024cf9dc29bfe78d352a9ea7eeea5f8cd25f1, weakHead=0x77cc3e39bc0c843f25c9556141f30e16901516a49dfb956fdc68e9c6f98592e1
```
The node (and other nodes) detected and accepted the re-org as part of
canonical chain
```

packages/beacon-node/src/chain/chain.ts
@@ -829,9 +829,9 @@ export class BeaconChain implements IBeaconChain {
     const timer = this.metrics?.forkChoice.findHead.startTimer({caller});
 
     try {
-      return this.forkChoice.updateAndGetHead({mode: UpdateHeadOpt.GetCanonicialHead}).head;
+      return this.forkChoice.updateAndGetHead({mode: UpdateHeadOpt.GetCanonicalHead}).head;
     } catch (e) {
-      this.metrics?.forkChoice.errors.inc({entrypoint: UpdateHeadOpt.GetCanonicialHead});
+      this.metrics?.forkChoice.errors.inc({entrypoint: UpdateHeadOpt.GetCanonicalHead});
       throw e;
     } finally {
       timer?.();

packages/beacon-node/src/chain/prepareNextSlot.ts
@@ -136,7 +136,7 @@ export class PrepareNextSlotScheduler {
 
           // If we predict we can reorg, update prepareState with proposer head block
           if (proposerHeadRoot !== headRoot || proposerHeadSlot !== headSlot) {
-            this.logger.verbose("Weak head detected. May build on this block instead:", {
+            this.logger.verbose("Weak head detected. May build on parent block instead", {
               proposerHeadSlot,
               proposerHeadRoot,
               headSlot,

packages/beacon-node/src/metrics/metrics/beacon.ts
@@ -128,7 +128,7 @@ export function createBeaconMetrics(register: RegistryMetricCreator) {
         name: "beacon_fork_choice_indices_count",
         help: "Current count of indices in fork choice data structures",
       }),
-      notReorgedReason: register.gauge<{reason: NotReorgedReason}>({
+      notReorgedReason: register.counter<{reason: NotReorgedReason}>({
         name: "beacon_fork_choice_not_reorged_reason_total",
         help: "Reason why the current head is not re-orged out",
         labelNames: ["reason"],

packages/fork-choice/src/forkChoice/forkChoice.ts
@@ -63,13 +63,13 @@ export type ForkChoiceOpts = {
 };
 
 export enum UpdateHeadOpt {
-  GetCanonicialHead = "getCanonicialHead", // Skip getProposerHead
+  GetCanonicalHead = "getCanonicalHead", // Skip getProposerHead
   GetProposerHead = "getProposerHead", // With getProposerHead
   GetPredictedProposerHead = "getPredictedProposerHead", // With predictProposerHead
 }
 
 export type UpdateAndGetHeadOpt =
-  | {mode: UpdateHeadOpt.GetCanonicialHead}
+  | {mode: UpdateHeadOpt.GetCanonicalHead}
   | {mode: UpdateHeadOpt.GetProposerHead; secFromSlot: number; slot: Slot}
   | {mode: UpdateHeadOpt.GetPredictedProposerHead; secFromSlot: number; slot: Slot};
 
@@ -205,22 +205,22 @@ export class ForkChoice implements IForkChoice {
   } {
     const {mode} = opt;
 
-    const canonicialHeadBlock = mode === UpdateHeadOpt.GetPredictedProposerHead ? this.getHead() : this.updateHead();
+    const canonicalHeadBlock = mode === UpdateHeadOpt.GetPredictedProposerHead ? this.getHead() : this.updateHead();
     switch (mode) {
       case UpdateHeadOpt.GetPredictedProposerHead:
-        return {head: this.predictProposerHead(canonicialHeadBlock, opt.secFromSlot, opt.slot)};
+        return {head: this.predictProposerHead(canonicalHeadBlock, opt.secFromSlot, opt.slot)};
       case UpdateHeadOpt.GetProposerHead: {
         const {
           proposerHead: head,
           isHeadTimely,
           notReorgedReason,
-        } = this.getProposerHead(canonicialHeadBlock, opt.secFromSlot, opt.slot);
+        } = this.getProposerHead(canonicalHeadBlock, opt.secFromSlot, opt.slot);
         return {head, isHeadTimely, notReorgedReason};
       }
-      case UpdateHeadOpt.GetCanonicialHead:
-        return {head: canonicialHeadBlock};
+      case UpdateHeadOpt.GetCanonicalHead:
+        return {head: canonicalHeadBlock};
       default:
-        return {head: canonicialHeadBlock};
+        return {head: canonicalHeadBlock};
     }
   }
 
@@ -296,15 +296,14 @@ export class ForkChoice implements IForkChoice {
    *
    */
   predictProposerHead(headBlock: ProtoBlock, secFromSlot: number, currentSlot: Slot): ProtoBlock {
+    const {proposerBoost, proposerBoostReorg} = this.opts ?? {};
     // Skip re-org attempt if proposer boost (reorg) are disabled
-    if (!this.opts?.proposerBoost || !this.opts?.proposerBoostReorg) {
-      this.logger?.verbose("No proposer boot reorg prediction since the related flags are disabled");
-      return headBlock;
-    }
-
-    if (headBlock.slot === currentSlot) {
-      // If head block aka the head cache is current, that means `updateHead` is called during gossip handling,
-      // that can only happen if shouldOverrideForkChoiceUpdate = false so no reorg
+    if (!proposerBoost || !proposerBoostReorg) {
+      this.logger?.verbose("No proposer boost reorg prediction since the related flags are disabled", {
+        slot: currentSlot,
+        proposerBoost,
+        proposerBoostReorg,
+      });
       return headBlock;
     }
 
@@ -313,11 +312,18 @@ export class ForkChoice implements IForkChoice {
 
     if (result.shouldOverrideFcu) {
       this.logger?.verbose("Current head is weak. Predicting next block to be built on parent of head.", {
-        blockRoot,
         slot: currentSlot,
+        proposerHead: result.parentBlock.blockRoot,
+        weakHead: blockRoot,
       });
       return result.parentBlock;
     }
+
+    this.logger?.verbose("Current head is strong. Predicting next block to be built on head", {
+      slot: currentSlot,
+      head: headBlock.blockRoot,
+      reason: result.reason,
+    });
 
     return headBlock;
   }
@@ -339,8 +345,13 @@ export class ForkChoice implements IForkChoice {
     let proposerHead = headBlock;
 
     // Skip re-org attempt if proposer boost (reorg) are disabled
-    if (!this.opts?.proposerBoost || !this.opts?.proposerBoostReorg) {
-      this.logger?.verbose("No proposer boot reorg attempt since the related flags are disabled");
+    const {proposerBoost, proposerBoostReorg} = this.opts ?? {};
+    if (!proposerBoost || !proposerBoostReorg) {
+      this.logger?.verbose("No proposer boost reorg attempt since the related flags are disabled", {
+        slot,
+        proposerBoost,
+        proposerBoostReorg,
+      });
       return {proposerHead, isHeadTimely, notReorgedReason: NotReorgedReason.ProposerBoostReorgDisabled};
     }
 
@@ -400,7 +411,11 @@ export class ForkChoice implements IForkChoice {
     }
 
     // Reorg if all above checks fail
-    this.logger?.info("Will perform single-slot reorg to reorg out current weak head");
+    this.logger?.verbose("Performing single-slot reorg to remove current weak head", {
+      slot,
+      proposerHead: parentBlock.blockRoot,
+      weakHead: headBlock.blockRoot,
+    });
     proposerHead = parentBlock;
 
     return {proposerHead, isHeadTimely};